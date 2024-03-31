88ddf75139a086a1bf7028cd6227e4995b16e5b2
sulnong <iterator_sl@qq.com>
2024-03-31 17:33:46 +0800

    add spa page

---
+++ src/pages/disease/spa.vue
@@ -0,0 +1,63 @@
+<template>
+  <view class="main">
+    <Header title="脊柱关节炎诊断" />
+    <view class="diagnose-standard">
+      <view class="title"> 强直性脊柱炎诊断标准 </view>
+      <view class="desc">(修订的纽约标准，1984年)</view>
+    </view>
+    <view class="content">
+      <form>
+        <DiseaseRadio title="1. 下背痛的病程至少3个月，疼痛随活动改善，休息不缓解" v-model="spaValue.indicator1" />
+        <DiseaseRadio title="2. 腰椎在前、后屈和侧屈方向活动受限" v-model="spaValue.indicator2" />
+        <DiseaseRadio title="3. 胸廓扩张范围小于同年龄和同性别者的正常值" v-model="spaValue.indicator3" />
+        <DiseaseRadio title="4. 双侧骶髂关节炎2-4级" desc="（0=正常，1=可疑，2=轻度关节面破坏，硬化，3=中度关节间隙增宽，狭窄，部分强直，4=全部强直）"
+          :option-number="3" v-model="spaValue.indicator4" />
+      </form>
+    </view>
+    <button @click="print">打印测试</button>
+    <view class="button">
+      <view class="submit">患者注册</view>
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+import { ref, reactive } from 'vue'
+import Header from '../../components/header.vue'
+import Date from '../../components/date.vue'
+import DiseaseRadio from './disease-radio.vue';
+const value = ref([])
+const test = ref(0)
+const handleChange = (value: any) => {
+  console.log(value)
+}
+
+const print = () => {
+  console.log(spaValue.indicator1)
+  console.log('test: ', test.value)
+}
+// 该疾病下指标1-8, 0为无，1为有，2为未做
+const spaValue = reactive({
+  indicator1: 0,
+  indicator2: 0,
+  indicator3: 0,
+  indicator4: 0,
+  indicator5: 0,
+  indicator6: 0,
+  indicator7: 0,
+  indicator8: 0,
+})
+
+</script>
+
+<script lang="ts">
+export default {
+  options: {
+    styleIsolation: 'shared',
+  },
+}
+</script>
+
+<style lang="scss">
+@import './spa.scss';
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,27 +1,29 @@
 <template>
   <view class="main">
-    <view class="status_bar">
+    <Header />
+    <!-- <view class="status_bar">
       <view :style="{ 'height': statusBarHeight + 'px' }"></view>
       <view :style="{ 'height': customNavBarHeight + 'px' }" class="title">首页</view>
-      <!-- 搜索框 -->
-      <view class="search">
-        <uni-easyinput :styles="searchStyles" prefixIcon="search" placeholder="搜索" />
-        <view class="filter-btn">
-          <text>筛选</text>
-          <uni-icons type="bars" color="white"></uni-icons>
-        </view>
-
+    <view class="search">
+      <uni-easyinput :styles="searchStyles" prefixIcon="search" placeholder="搜索" />
+      <view class="filter-btn">
+        <text>筛选</text>
+        <uni-icons type="bars" color="white"></uni-icons>
       </view>
+
     </view>
+  </view> -->
     <!-- 录入: 拍照录入 与 手动录入 -->
     <view class="input-area">
       <view class="camera-input">
         <text class="uno-text-base">拍照录入</text>
         <text class="uno-text-sm uno-opacity-50 uno-mt-1">拍照识别录入</text>
+        <image class="bg" src="../../static/img/path.png"></image>
       </view>
       <view class="manual-input">
         <text class="uno-text-base">手动录入</text>
         <text class="uno-text-sm uno-opacity-50 uno-mt-1">填写添加患者</text>
+        <image class="bg" src="../../static/img/edit.png"></image>
       </view>
     </view>
     <!-- 病人信息列表 -->
@@ -31,7 +33,7 @@
         <view class="line-one">
           <!-- 第一行: 病人姓名，随诊次数 -->
           <text class="name">{{ item.name }}</text>
-          <wd-tag type="success" mark class="uno-opacity-80 uno-w-1 uno-h-1">{{ item.diagTimes }}次随诊
+          <wd-tag type="success" round custom-class="uno-opacity-80 uno-ml-2 uno-center">{{ item.diagTimes }}次随诊
           </wd-tag>
           <view class="line-three"></view>
         </view>
@@ -43,9 +45,10 @@
         </view>
         <!-- 电话/短信提醒 -->
         <view class="action uno-flex-content-between">
-          <wd-button plain icon="" @click="callPatient(item)">拨打电话</wd-button>
-          <wd-button plain icon="" @click="messagePatient(item)">短信提醒</wd-button>
-          <wd-button plain icon="" @click="messagePatient(item)">随访</wd-button>
+          <wd-button plain size="small" icon="user-avatar" @click="messagePatient(item)">随诊</wd-button>
+          <wd-button plain size="small" icon="" @click="messagePatient(item)">短信提醒</wd-button>
+          <wd-button plain size="small" icon="call" @click="callPatient(item)">拨打电话</wd-button>
+          <wd-icon name="" size="22px"></wd-icon>
         </view>
       </wd-card>
     </view>
@@ -54,26 +57,24 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import { storeToRefs } from 'pinia'
-import { useGlobalStore } from '@/store'
 import { onReady } from '@dcloudio/uni-app';
-const store = useGlobalStore()
-const { sysInfo, customNavBarHeight, statusBarHeight, menuButtonRect } = storeToRefs(store)
+import Header from '@/components/header.vue'
+// 自定义导航栏代码，届时删除
+// import { storeToRefs } from 'pinia'
+// import { useGlobalStore } from '@/store'
+// const store = useGlobalStore()
+// const { sysInfo, customNavBarHeight, statusBarHeight, menuButtonRect } = storeToRefs(store)
+// const searchStyles = ref({
+//   backgroundColor: '#fff',
+//   borderRadius: '35px',
+// })
 
-const searchStyles = ref({
-  backgroundColor: '#fff',
-  borderRadius: '35px',
-})
 const patientList = ref([
   { id: 1, name: '韩可可', age: 31, sex: '男', daysToNextDiag: 3, diagTimes: 6 },
   { id: 2, name: '陈州', age: 33, sex: '男', daysToNextDiag: 10, diagTimes: 3 },
   { id: 3, name: '陈敏', age: 40, sex: '女', daysToNextDiag: -5, diagTimes: 2 },
 ])
 
-onReady(() => {
-  console.log(customNavBarHeight, statusBarHeight)
-  console.log('页面加载完成')
-})
 
 const callPatient = (item: any) => {
   console.log('拨打电话', item)
@@ -84,107 +85,6 @@ const messagePatient = (item: any) => {
 }
 </script>
 
-<style lang="scss">
-.main {
-  background-color: rgb(245, 245, 245);
-}
-
-// 导航栏
-.status_bar {
-  width: 100%;
-  height: 150px;
-  background: linear-gradient(180deg, rgba(30, 217, 167, 1) 0%, rgba(17, 194, 147, 1) 100%);
-  display: flex;
-  flex-direction: column;
-  color: #fff;
-  position: relative;
-
-  .title {
-    display: flex;
-    justify-content: start;
-    align-items: center;
-    font-size: 18px;
-    margin-left: 10px;
-  }
-
-  .search {
-    position: absolute;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    bottom: 0;
-    left: 0;
-    height: 50px;
-    width: 100%;
-    padding: 0 10px 5px 10px;
-
-    .filter-btn {
-      width: 80px;
-      height: 50px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-  }
-}
-
-// 拍照录入 与 手动录入
-.input-area {
-  display: flex;
-  justify-content: space-evenly;
-  padding: 10px;
-  color: #fff;
-
-  @mixin input-box {
-    width: 40%;
-    height: 60px;
-    display: flex;
-    flex-direction: column;
-    border-radius: 10px;
-    padding: 10px 0 0 10px;
-  }
-
-  .camera-input {
-    @include input-box();
-    background: rgba(62, 141, 246, 1);
-  }
-
-  .manual-input {
-    @include input-box();
-    background: rgba(2, 205, 150, 1);
-  }
-}
-
-.patient-list {
-  .item {
-    display: flex;
-    flex-direction: column;
-    height: 115px;
-
-    .line-one {
-      .name {
-        font-size: large;
-        color: black;
-        font-weight: 400;
-      }
-
-      display: flex;
-      height: 30px;
-    }
-
-    .line-two {
-      display: flex;
-      height: 30px;
-    }
-
-    .line-three {
-      display: flex;
-      height: 30px;
-    }
-  }
-
-  .action {
-    display: flex;
-  }
-}
+<style lang="scss" scoped>
+@import './index.scss';
 </style>
