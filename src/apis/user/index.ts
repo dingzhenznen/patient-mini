3ca544bf43736201c97ca8fd454b6e545173d4da
dingzhenznen <1324652616@qq.com>
2024-04-08 17:32:55 +0800

    fix

--- src/apis/user/index.ts
+++ src/apis/user/index.ts
@@ -20,14 +20,17 @@ export const initUserInfo = async (callback?: Function) => {
           data: { code },
           method: 'POST',
         })
-        const r = await list({ userId: user._id })
-        usePatientStore().setPatients(r.data)
+        // const r = await list({ userId: user._id })
+        // usePatientStore().setPatients(r.data)
         // console.log('获取病人列表: ', r.data)
         // console.log('store 中的病人列表: ', usePatientStore().patients)
         // 后台配置了手机号
         if (user.phone) {
           userStore.updateUserInfo(user)
           saveToken(access_token, expires_in)
+
+          const r = await list({ userId: user._id })
+          usePatientStore().setPatients(r.data)
           // 跳转首页
           uni.switchTab({ url: '/pages/index/index' })
           if (callback) {
@@ -35,7 +38,7 @@ export const initUserInfo = async (callback?: Function) => {
           }
         } else {
           userStore.updateUserInfo(user)
-          uni.switchTab({ url: '/pages/login/index' })
+          uni.navigateTo({ url: '/pages/login/index' })
         }
       },
       fail: () => {
